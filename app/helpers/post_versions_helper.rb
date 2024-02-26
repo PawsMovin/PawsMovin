e621ng/e621ng

[PostVersions] Fix index for frozen string literals

app/helpers/post_versions_helper.rb
@@ -1,70 +1,59 @@
 module PostVersionsHelper
   def post_source_diff(post_version)
     diff = post_version.diff_sources(post_version.previous)
-    html = '<span class="diff-list">'
+    changes = []
 
     diff[:added_sources].each do |source|
-      prefix = '<div><ins>+'
-      html << prefix + wordbreakify(source) + '</ins></div>'
-      html << " "
+      changes << tag.div(tag.ins(wordbreak_source("+#{source}")))
     end
     diff[:removed_sources].each do |source|
-      prefix = '<div><del>-'
-      html << prefix + wordbreakify(source) + '</del></div>'
-      html << " "
+      changes << tag.div(tag.del(wordbreak_source("-#{source}")))
     end
     diff[:unchanged_sources].each do |source|
-      html << '<div>' + wordbreakify(source) + '</div>'
-      html << " "
+      changes << tag.div(wordbreak_source(source))
     end
 
-    html << "</span>"
-    html.html_safe
+    tag.span(safe_join(changes, " "), class: "diff-list")
+  end
+
+  def wordbreak_source(string)
+    lines = string.scan(/.{1,10}/)
+    safe_join(lines, tag.wbr)
   end
 
   def post_version_diff(post_version)
     diff = post_version.diff(post_version.previous)
-    html = '<span class="diff-list">'
+    changes = []
 
-    diff[:added_tags].each do |tag|
-      prefix = diff[:obsolete_added_tags].include?(tag) ? '<ins class="obsolete">+' : '<ins>+'
-      html << prefix + link_to(tag, posts_path(:tags => tag)) + '</ins>'
-      html << " "
+    diff[:added_tags].each do |tag_name|
+      classes = diff[:obsolete_added_tags].include?(tag_name) ? "obsolete" : ""
+      changes << tag.ins(link_to("+#{tag_name}", posts_path(tags: tag_name), class: classes))
     end
-    diff[:removed_tags].each do |tag|
-      prefix = diff[:obsolete_removed_tags].include?(tag) ? '<del class="obsolete">-' : '<del>-'
-      html << prefix + link_to(tag, posts_path(:tags => tag)) + '</del>'
-      html << " "
+    diff[:removed_tags].each do |tag_name|
+      classes = diff[:obsolete_removed_tags].include?(tag_name) ? "obsolete" : ""
+      changes << tag.del(link_to("-#{tag_name}", posts_path(tags: tag_name), class: classes))
     end
-    diff[:unchanged_tags].each do |tag|
-      html << '<span>' + link_to(tag, posts_path(:tags => tag)) + '</span>'
-      html << " "
+    diff[:unchanged_tags].each do |tag_name|
+      changes << tag.span(link_to(tag_name, posts_path(tags: tag_name)))
     end
 
-    html << "</span>"
-    html.html_safe
+    tag.span(safe_join(changes, " "), class: "diff-list")
   end
 
   def post_version_locked_diff(post_version)
     diff = post_version.diff(post_version.previous)
-    html = '<span class="diff-list">'
+    changes = []
 
-    diff[:added_locked_tags].each do |tag|
-      prefix = '<ins>+'
-      html << prefix + link_to(tag, posts_path(:tags => tag)) + '</ins>'
-      html << " "
+    diff[:added_locked_tags].each do |tag_name|
+      changes << tag.ins(link_to("+#{tag_name}", posts_path(tags: tag_name)))
     end
-    diff[:removed_locked_tags].each do |tag|
-      prefix = '<del>-'
-      html << prefix + link_to(tag, posts_path(:tags => tag)) + '</del>'
-      html << " "
+    diff[:removed_locked_tags].each do |tag_name|
+      changes << tag.del(link_to("-#{tag_name}", posts_path(tags: tag_name)))
     end
     diff[:unchanged_locked_tags].each do |tag|
-      html << '<span>' + link_to(tag, posts_path(:tags => tag)) + '</span>'
-      html << " "
+      changes << tag.span(link_to(tag_name, posts_path(tags: tag_name)))
     end
 
-    html << "</span>"
-    html.html_safe
+    tag.span(safe_join(changes, " "), class: "diff-list")
   end
 end

PawsMovin/PawsMovin

app/helpers/post_versions_helper.rb
@@ -1,70 +1,59 @@
 module PostVersionsHelper
   def post_source_diff(post_version)
     diff = post_version.diff_sources(post_version.previous)
-    html = '<span class="diff-list">'
+    changes = []
 
     diff[:added_sources].each do |source|
-      prefix = '<div><ins>+'
-      html << prefix + wordbreakify(source) + '</ins></div>'
-      html << " "
+      changes << tag.div(tag.ins(wordbreak_source("+#{source}")))
     end
     diff[:removed_sources].each do |source|
-      prefix = '<div><del>-'
-      html << prefix + wordbreakify(source) + '</del></div>'
-      html << " "
+      changes << tag.div(tag.del(wordbreak_source("-#{source}")))
     end
     diff[:unchanged_sources].each do |source|
-      html << '<div>' + wordbreakify(source) + '</div>'
-      html << " "
+      changes << tag.div(wordbreak_source(source))
     end
 
-    html << "</span>"
-    html.html_safe
+    tag.span(safe_join(changes, " "), class: "diff-list")
+  end
+
+  def wordbreak_source(string)
+    lines = string.scan(/.{1,10}/)
+    safe_join(lines, tag.wbr)
   end
 
   def post_version_diff(post_version)
     diff = post_version.diff(post_version.previous)
-    html = '<span class="diff-list">'
+    changes = []
 
-    diff[:added_tags].each do |tag|
-      prefix = diff[:obsolete_added_tags].include?(tag) ? '<ins class="obsolete">+' : '<ins>+'
-      html << prefix + link_to(tag, show_or_new_wiki_pages_path(title: tag)) + '</ins>'
-      html << " "
+    diff[:added_tags].each do |tag_name|
+      classes = diff[:obsolete_added_tags].include?(tag_name) ? "obsolete" : ""
+      changes << tag.ins(link_to("+#{tag_name}", show_or_new_wiki_pages_path(title: tag_name), class: classes))
     end
-    diff[:removed_tags].each do |tag|
-      prefix = diff[:obsolete_removed_tags].include?(tag) ? '<del class="obsolete">-' : '<del>-'
-      html << prefix + link_to(tag, show_or_new_wiki_pages_path(title: tag)) + '</del>'
-      html << " "
+    diff[:removed_tags].each do |tag_name|
+      classes = diff[:obsolete_removed_tags].include?(tag_name) ? "obsolete" : ""
+      changes << tag.del(link_to("-#{tag_name}", show_or_new_wiki_pages_path(title: tag_name), class: classes))
     end
-    diff[:unchanged_tags].each do |tag|
-      html << '<span>' + link_to(tag, show_or_new_wiki_pages_path(title: tag)) + '</span>'
-      html << " "
+    diff[:unchanged_tags].each do |tag_name|
+      changes << tag.span(link_to(tag_name, show_or_new_wiki_pages_path(title: tag_name)))
     end
 
-    html << "</span>"
-    html.html_safe
+    tag.span(safe_join(changes, " "), class: "diff-list")
   end
 
   def post_version_locked_diff(post_version)
     diff = post_version.diff(post_version.previous)
-    html = '<span class="diff-list">'
+    changes = []
 
-    diff[:added_locked_tags].each do |tag|
-      prefix = '<ins>+'
-      html << prefix + link_to(tag, show_or_new_wiki_pages_path(title: tag)) + '</ins>'
-      html << " "
+    diff[:added_locked_tags].each do |tag_name|
+      changes << tag.ins(link_to("+#{tag_name}", show_or_new_wiki_pages_path(title: tag_name)))
     end
-    diff[:removed_locked_tags].each do |tag|
-      prefix = '<del>-'
-      html << prefix + link_to(tag, show_or_new_wiki_pages_path(title: tag)) + '</del>'
-      html << " "
+    diff[:removed_locked_tags].each do |tag_name|
+      changes << tag.del(link_to("-#{tag_name}", show_or_new_wiki_pages_path(title: tag_name)))
     end
-    diff[:unchanged_locked_tags].each do |tag|
-      html << '<span>' + link_to(tag, show_or_new_wiki_pages_path(title: tag)) + '</span>'
-      html << " "
+    diff[:unchanged_locked_tags].each do |tag_name|
+      changes << tag.span(link_to(tag_name, show_or_new_wiki_pages_path(title: tag)))
     end
 
-    html << "</span>"
-    html.html_safe
+    tag.span(safe_join(changes, " "), class: "diff-list")
   end
 end