063fdbecdc4fb938fd5f0952bcad5bdde97ccb64
Donovan Daniels <hewwo@yiff.rocks>
2024-05-12 20:53:13 -0500

    Only allow Owner users to assign can_manage_aibur

--- app/policies/admin/user_policy.rb
+++ app/policies/admin/user_policy.rb
@@ -27,8 +27,9 @@ module Admin
     end
 
     def permitted_attributes
-      attr = %i[profile_about profile_artinfo base_upload_limit level enable_privacy_mode unrestricted_uploads can_approve_posts no_flagging no_replacements can_manage_aibur no_aibur_voting force_name_change]
-      attr += %i[email title] if user.is_owner?
+      attr = %i[profile_about profile_artinfo base_upload_limit enable_privacy_mode unrestricted_uploads can_approve_posts no_flagging no_replacements no_aibur_voting force_name_change]
+      attr += %i[can_manage_aibur email title] if user.is_owner?
+      attr += %i[level] if user.is_owner? || !record.is_admin?
       attr
     end
   end
