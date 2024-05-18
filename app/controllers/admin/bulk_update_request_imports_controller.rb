f910011d9569426960632fda12cac56c57edd908
Donovan Daniels <hewwo@yiff.rocks>
2024-05-18 04:44:07 -0500

    Add error handling to bulk update request import controller

--- app/controllers/admin/bulk_update_request_imports_controller.rb
+++ app/controllers/admin/bulk_update_request_imports_controller.rb
@@ -9,7 +9,17 @@ module Admin
     def create
       bparams = params[:batch].presence || params
       @importer = authorize(BulkUpdateRequestImporter.new(bparams[:script], bparams[:forum_id]))
-      @importer.process!
+      begin
+        BulkUpdateRequest.transaction { @importer.process! }
+      rescue BulkUpdateRequestImporter::Error => e
+        @error = e
+        notice("Import failed")
+        respond_to do |format|
+          format.html { render(:new) }
+          format.json { render_expected_error(400, e.message) }
+        end
+        return
+      end
       notice("Import queued")
       respond_to do |format|
         format.html { redirect_to(new_admin_bulk_update_request_import_path) }
